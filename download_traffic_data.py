9ef4b7dd15961417ef66e26da33fdc5779fde3a4
pikachu1mv <shj3374@gmail.com>
2023-11-25 23:01:21 +0900

    classed

--- download_traffic_data.py
+++ download_traffic_data.py
@@ -3,20 +3,23 @@ import util.decompressor as decompressor
 import os
 
 path = './data/raw_data/traffic/'
-url = 'https://www.its.go.kr/opendata/fileDownload/traffic/2023/'
-target_list = [f'202309{i:02}_5Min.zip' for i in range(1, 31)]
+target_list = [f'202306{i:02}_5Min.zip' for i in range(1, 3)]
 
-path_list = [path + i for i in target_list]
-url_list = [url + i for i in target_list]
+class its_downloader(downloader.FileDownloader):
+    def __init__(self, thread_num=2, download_unit=1024*32):
+        super(its_downloader, self).__init__(thread_num=thread_num, download_unit=download_unit)
+        self.baseurl = 'https://www.its.go.kr/opendata/fileDownload/traffic/2023/'
+        self.decompressor = decompressor.Decompressor(thread_num=thread_num)
 
-print(url_list)
+    def get_data(self, srcpath, destpath, target_list):
+        path_list = [srcpath + i for i in target_list]
+        url_list = [self.baseurl + i for i in target_list]
 
-dwn = downloader.FileDownloader(thread_num=2)
-dwn.download(path_list, url_list)
+        self.download(path_list, url_list)
+        self.decompressor.decompress(path_list, [destpath]*len(target_list))
 
-dcmp = decompressor.Decompressor(thread_num=4)
-dcmp.decompress(path_list, [path]*len(path_list))
+        for i in path_list:
+            os.remove(i)
 
-# remove zip
-for i in path_list:
-    os.remove(i)
+t = its_downloader()
+t.get_data(path, path, target_list)
